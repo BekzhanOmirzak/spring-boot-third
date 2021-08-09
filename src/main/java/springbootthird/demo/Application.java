7ec5efdc9a437767eae5739b35da719196d88d62
Bekzhan <bekjan.omirzak98@gmail.com>
2021-08-09 13:32:07 +0600

    First Commit

---
+++ src/main/java/springbootthird/demo/Application.java
@@ -0,0 +1,24 @@
+package springbootthird.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+
+    @GetMapping
+    public String getString() {
+        return "This is a new string from";
+    }
+
+
+}
---
+++ src/test/java/springbootthird/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootthird.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
